4073bb57ccc99b198f5f27ebec9ca2f65494ac79
noenik <niklas.noem@gmail.com>
2015-03-02 15:45:08 +0100

    Initial

---
+++ processing_java_base/src/processing_java_base/SampleSketch.java
@@ -0,0 +1,49 @@
+package processing_java_base;
+
+import processing.core.*;
+
+/**
+ *
+ * @author nikla_000
+ */
+public class SampleSketch extends PApplet {
+
+//    float posX = 200;
+//    float posY = 200;
+    Particle x;
+    Particle z;
+
+    @Override
+    public void setup() {
+        size(400, 400);
+        x = new Particle(200, 200, 20, 20);
+        z = new Particle(200, 200, 20, 20);
+        frameRate(10);
+    }
+
+    @Override
+    public void draw() {
+        background(0);
+        stroke(255);
+
+        float xCoord = 1;
+        float yCoord = 1;
+        for (float y = 1; y < width; y++) {
+            line(0, yCoord, width, yCoord);
+                line(xCoord, 0, xCoord, height);
+                xCoord += 40;
+                yCoord += 40;
+        }
+
+        line(width / 2, 0, width / 2, height);
+        line(0, height / 2, width, height / 2);
+
+        x.move(random(2) - 1, random(2) - 1);
+        z.move(random(2) - 1, random(2) - 1);
+//        posY += random(2) - 1;
+        fill(255, 0, 0);
+        ellipse(x.getxCoord(), x.getyCoord(), x.getWidth(), x.getHeight());
+        ellipse(z.getxCoord(), z.getyCoord(), z.getWidth(), z.getHeight());
+    }
+
+}
