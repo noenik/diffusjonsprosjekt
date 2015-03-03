bca7b0ef7c8045794157575c85c2d2faf9b16c6a
Ramin <ramin_esfandiari_93@hotmail.com>
2015-03-03 08:55:23 +0100

    Endret navn på klasser til noe mer passende.

---
+++ processing_java_base/src/Diffusion_Project/DisplayFrame.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Diffusion_Project;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+/**
+ * @author nikla_000
+ */
+public class DisplayFrame extends JFrame
+{
+    private ProcessingDraw s;
+
+    private JPanel panel;
+    //    private JLabel label;
+
+    public DisplayFrame ()
+    {
+        this.setSize (600, 600);
+        setDefaultCloseOperation (WindowConstants.EXIT_ON_CLOSE);
+
+        panel = new JPanel ();
+        //        label = new JLabel();
+        s = new ProcessingDraw ();
+
+        panel.add (s);
+        this.add (panel);
+        //        this.add(label);
+
+
+        //        setLayout(null);
+        //        label.setBounds(200, 550, 100, 30);
+
+        s.init ();
+
+        this.setVisible (true);
+    }
+}
