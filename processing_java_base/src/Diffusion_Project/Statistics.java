751f7bef1d0ded1565a37a60fdfe5c971e6cde2f
Ramin <ramin_esfandiari_93@hotmail.com>
2015-03-03 15:57:01 +0100

    Tilpasset statastics klassen

--- processing_java_base/src/Diffusion_Project/Statistics.java
+++ processing_java_base/src/Diffusion_Project/Statistics.java
@@ -9,9 +9,15 @@ public class Statistics
 {
     private int numberOfStepIn1D;
     private int dimension;
-    private int diffusionCoeffisient;
+    private int diffusionCoefficient;
     private int time;
     private int constant = 2;
+
+    private double standardDeviation;
+    private double variance;
+    private double sampleMean;
+
+    private Animate animate;
 
     ArrayList<Particle> particleList;
 
@@ -19,8 +25,9 @@ public class Statistics
 
     public Statistics()
     {
+        animate = new Animate();
         particle = new Particle();
-
+        this.time = animate.getTime();
     }
 
     public void setParticleList(ArrayList<Particle> particleList)
@@ -42,5 +49,35 @@ public class Statistics
     public void setParticle(Particle particle)
     {
         this.particle = particle;
+    }
+
+    public double getVariance()
+    {
+        return variance;
+    }
+
+    public void setVariance(double variance)
+    {
+        this.variance = variance;
+    }
+
+    public double getStandardDeviation()
+    {
+        return standardDeviation = Math.sqrt( getVariance() );
+    }
+
+    public void setStandardDeviation(double standardDeviation)
+    {
+        this.standardDeviation = standardDeviation;
+    }
+
+    public double getSampleMean()
+    {
+        return Math.signum( getNumberOfStepIn1D() ) / time;
+    }
+
+    public void setSampleMean(double sampleMean)
+    {
+        this.sampleMean = sampleMean;
     }
 }
