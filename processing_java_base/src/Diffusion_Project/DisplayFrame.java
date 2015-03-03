f5263ed523f6abb1fd005325de6fd256babd5d44
Ramin <ramin_esfandiari_93@hotmail.com>
2015-03-03 15:11:49 +0100

    Tilpasset statastics klassen

--- processing_java_base/src/Diffusion_Project/DisplayFrame.java
+++ processing_java_base/src/Diffusion_Project/DisplayFrame.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Diffusion_Project;
 
 import javax.swing.*;
@@ -69,6 +65,5 @@ public class DisplayFrame extends JFrame
 
         button.addActionListener( animate );
         button.setActionCommand( "custom" );
-
     }
 }
